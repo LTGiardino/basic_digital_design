02436906ed9fcff56a9ed39141339f9fed148ae2
Lucas Giardino <ltgiardino@gmail.com>
2021-03-04 23:01:44 -0300

    document test bench

--- example_1/tb_shiftleds.v
+++ example_1/tb_shiftleds.v
@@ -1,101 +1,112 @@
-// TB
+// Test-Bench of module:
 // ShiftLeds
-`define N_LEDS 4
-`define NB_SEL 2
-`define NB_COUNT 14
-`define NB_SW 4
-`define NB_BTN 4
+`define N_LEDS   4
+`define NB_SEL   2
+`define NB_COUNT 14 // I use a lower count number b/c I really don't need 32bit
+`define NB_SW    4
+`define NB_BTN   4
 
+// Set the timescale to emulate Artix-7 board
 `timescale 1ns/100ps
 
 module tb_shiftleds();
+	parameter N_LEDS   = `N_LEDS   ;
+	parameter NB_SEL   = `NB_SEL   ;
+	parameter NB_COUNT = `NB_COUNT ;
+	parameter NB_SW    = `NB_SW    ;
+	parameter NB_BTN   = `NB_BTN   ;
 
-   parameter N_LEDS   = `N_LEDS   ;
-   parameter NB_SEL   = `NB_SEL   ;
-   parameter NB_COUNT = `NB_COUNT ;
-   parameter NB_SW    = `NB_SW    ;
-   parameter NB_BTN    = `NB_BTN    ;
+	// I select all outputs as wires and all inputs as registers.
+	// b/c i need them to be stored somewehere, right?
+	wire [NB_BTN-1:0]   o_led    ;
+	wire [N_LEDS-1:0]   o_led_r  ;
+	wire [N_LEDS-1:0]   o_led_b  ;
+	wire [N_LEDS-1:0]   o_led_g  ;
+	reg  [NB_SW-1 :0]   i_sw     ;
+	reg  [NB_BTN-1:0]   i_btn    ;
+	reg                 i_reset  ;
+	reg                 clock    ;
+	wire [NB_COUNT-1:0] tb_count ;
 
-   wire [NB_BTN - 1 : 0] o_led     ;
-   wire [N_LEDS - 1 : 0] o_led_r  ;
-   wire [N_LEDS - 1 : 0] o_led_b  ;
-   wire [N_LEDS - 1 : 0] o_led_g  ;
-   reg  [NB_SW   - 1 : 0] i_sw     ;
-   reg  [NB_BTN  - 1 : 0] i_btn   ;
-   reg                   i_reset  ;
-   reg                   clock;
+	// get a scope point to register counter value on the waveform
+	assign tb_count = tb_shiftleds.u_shiftleds.counter;
 
-   wire [NB_COUNT - 1 : 0] tb_count;
+	// Run initial to set everything ONCE at first and that's it
+	initial begin
+		// inicialize inputs
+		i_sw[0]    = 1'b0       ; // i_sw[0] = ENABLE
+		clock      = 1'b0       ;
+		i_reset    = 1'b0       ; // do not reset yet
+		i_sw[2:1]  = `NB_SEL'h0 ; // all other switches on 0
+		i_sw[3]    = 1'b0       ;
+		i_btn[3:0] = 4'b0000    ; // all buttons not pressed
 
-   assign tb_count = tb_shiftleds.u_shiftleds.counter;
-     
-      
-   initial begin
-      i_sw[0]      = 1'b0  ;
-      clock        = 1'b0  ;
-      i_reset      = 1'b0  ;
-      i_sw[2:1]    = `NB_SEL'h0 ;
-      i_sw[3]      = 1'b0  ;
-      i_btn[3:0]    = 4'b0000;
-      
-      #100 i_reset = 1'b1  ;
-      #100 i_sw[0] = 1'b1  ;
-      
-      //force tb_shiftleds.u_shiftleds.o_led = 4'b0001;
-      #1000 i_btn[0] = 1'b1;
-      #100 i_btn[0] = 1'b0;
-      
-      #1000 i_btn[1] = 1'b1;
-      #100 i_btn[1] = 1'b0;
-      
-      #1000 i_btn[0] = 1'b1;
-      #100 i_btn[0] = 1'b0;
-      
-      #1000 i_btn[2] = 1'b1;
-      #100 i_btn[2] = 1'b0;
-      
-      #1000 i_btn[0] = 1'b1;
-      #100 i_btn[0] = 1'b0;
-      
-      #1000 i_btn[3] = 1'b1;
-      #100 i_btn[3] = 1'b0;
-      
-      #1000 i_btn[0] = 1'b1;
-      #100 i_btn[0] = 1'b0;
-      // Cambio de SR a FS
-      // Arranca a Flashear el led rojo
-      
-      //#50000 i_btn[0] = 1'b1;
-      //#100 i_btn[0] = 1'b0;
-      // Cambio de FS a SR
-      // Dejo de flashear el rojo y hace la secuencia para adelante
-      
-//      #1000000 i_sw[2:1]  = `NB_SEL'h1 ;
-//      #1000000 i_sw[2:1]  = `NB_SEL'h2 ;
-//      #1000000 i_sw[3]    = 1'b1       ;
-//      #1000000 i_sw[2:1]  = `NB_SEL'h3 ;
-      #1000 $finish;
-   end
+		// wait 100 clocks and set enable and reset
+		#100 i_reset = 1'b1;
+		#100 i_sw[0] = 1'b1;
 
-   always #5 clock = ~clock;
+		// Begin pressing buttons for 100 clocks (simulating a push and
+		// release of a real button).
+		// This should probably be better off written on an external file
 
-shiftleds
-  #(
-    .N_LEDS   (N_LEDS  ),
-    .NB_SEL   (NB_SEL  ),
-    .NB_COUNT (NB_COUNT),
-    .NB_SW    (NB_SW   )
-    )
-  u_shiftleds
-    (
-     .o_led     (o_led  )  ,
-     .o_led_r   (o_led_r)  ,
-     .o_led_b   (o_led_b)  ,
-     .o_led_g   (o_led_g)  ,
-     .i_sw      (i_sw   )  ,
-     .i_btn     (i_btn) ,
-     .ck_rst   (i_reset)  ,
-     .clock     (clock  )
-     );
+		// Change mode. SR -> Flash
+		#1000 i_btn[0] = 1'b1;
+		#100  i_btn[0] = 1'b0;
+
+		// Enable RED color (this should already be enabled so nothing must
+		// happen)
+		#1000 i_btn[1] = 1'b1;
+		#100  i_btn[1] = 1'b0;
+
+		// Change mode. Flash -> SR
+		#1000 i_btn[0] = 1'b1;
+		#100  i_btn[0] = 1'b0;
+
+		// Enable GREEN color.
+		#1000 i_btn[2] = 1'b1;
+		#100  i_btn[2] = 1'b0;
+
+		// Change mode. SR -> Flash
+		#1000 i_btn[0] = 1'b1;
+		#100  i_btn[0] = 1'b0;
+
+		// Enable BLUE color
+		#1000 i_btn[3] = 1'b1;
+		#100  i_btn[3] = 1'b0;
+
+		// Change mode. Flash -> SR
+		#1000 i_btn[0] = 1'b1;
+		#100  i_btn[0] = 1'b0;
+
+		// Wait and finish
+		#1000 $finish;
+	end
+
+	// Select the clock frecuency 5clocks HIGH 5clocks LOW.
+	// each clock is 1ns, so 10ns clock period
+	always #5 clock = ~clock;
+
+	// Instance shiftleds module
+	shiftleds
+		// Set the global vraiables
+	  #(
+		  .N_LEDS   (N_LEDS  ),
+		  .NB_SEL   (NB_SEL  ),
+		  .NB_COUNT (NB_COUNT),
+		  .NB_SW    (NB_SW   )
+	  )
+	  // select instance name and connect all end-points to test-bench
+	  // registers/wires
+	  u_shiftleds
+	  (
+		  .o_led     (o_led  ),
+		  .o_led_r   (o_led_r),
+		  .o_led_b   (o_led_b),
+		  .o_led_g   (o_led_g),
+		  .i_sw      (i_sw   ),
+		  .i_btn     (i_btn  ),
+		  .ck_rst    (i_reset),
+		  .clock     (clock  )
+	  );
 
 endmodule // tb_shiftleds
